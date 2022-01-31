fe47d027b02d027f963bd308041ed9c431f22dfc
Erhan <62097612+erhankabaoglu@users.noreply.github.com>
2022-01-31 17:12:44 +0300

    Update module.py

--- module.py
+++ module.py
@@ -1,4 +1,7 @@
 def pascalize(str):
+    /* Takes a string and finds parts seperated by blanks or underlines.
+       Capitalize each part and join end to end. */ 
+        
     temp = []
     if ' ' in str:
         temp = str.split()
@@ -11,6 +14,9 @@ def pascalize(str):
 
 
 def depascalize(str):
+    /* Takes a string and finds parts which are starting with upper latters.
+       Join these parts after converting to lower case and put underline between them. */
+        
     pos = [i for i, e in enumerate(str + 'A') if e.isupper()]
     parts = [str[pos[j]:pos[j + 1]] for j in range(len(pos) - 1)]
     lst = ""
@@ -20,6 +26,10 @@ def depascalize(str):
 
 
 def camelize(str):
+    /* Takes a string and remove dots in the string first if string has one or more dots except last character.
+       Finds parts seperated by blanks or underlines.
+       Capitalize each part except first part and join end to end. */
+        
     lst = str[-1]
     str_t = str[:-1].replace('.', '')
     if str[:-1] == str_t:
@@ -42,6 +52,9 @@ def camelize(str):
 
 
 def snakecase(str):
+    /* Takes a string and finds parts which are starting with upper latters.
+       Join parts after converting to lower case and put underlines between them. */
+        
     pos = [i for i, e in enumerate(str + 'A') if e.isupper()]
     parts = [str[pos[j]:pos[j + 1]] for j in range(len(pos) - 1)]
     rst = str[:pos[0]] + '_'
@@ -51,18 +64,24 @@ def snakecase(str):
 
 
 def is_pascalize(str):
+    /* Takes a string and check if string not contains blanks or underlines or start with lower case. */
+    
     if ' ' in str or '_' in str or str[0] != str[0].upper():
         return False
     return True
 
 
 def is_camelcase(str):
+    /* Takes a string and check if string not contains blanks or underlines or start with upper case. */
+    
     if ' ' in str or '_' in str or str[0] != str[0].lower():
         return False
     return True
 
 
 def is_snakecase(str):
+    /* Takes a string and check if string not contains blanks or start with upper case. */
+    
     if ' ' in str or str != str.lower():
         return False
     return True
